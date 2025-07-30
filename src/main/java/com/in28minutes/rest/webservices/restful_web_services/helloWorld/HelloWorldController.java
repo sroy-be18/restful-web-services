39638a659485dda54e58b818f0175e7bb350feba
saurabro <Saurabh.Roy@amdocs.com>
2025-07-31 02:02:52 +0530

    Content Negotiation Added

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloWorld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/helloWorld/HelloWorldController.java
@@ -1,10 +1,18 @@
 package com.in28minutes.rest.webservices.restful_web_services.helloWorld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api")
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping("/hello-world")
     public String hellWorld(){
@@ -19,5 +27,11 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean hellWorldPathVariable(@PathVariable("name") String name){
         return  new HelloWorldBean("Hello World, " + name);
+    }
+
+    @GetMapping("/hello-world-internationalized")
+    public String hellWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message",null,"Default Message",locale);
     }
 }
