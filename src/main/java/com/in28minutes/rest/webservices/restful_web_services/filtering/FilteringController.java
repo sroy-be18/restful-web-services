2e6a57d2d1a5543f37c404c2e00113b7b5d987a3
saurabro <Saurabh.Roy@amdocs.com>
2025-07-31 12:46:11 +0530

    Remove Import

--- src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/FilteringController.java
+++ src/main/java/com/in28minutes/rest/webservices/restful_web_services/filtering/FilteringController.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
-import org.jspecify.annotations.Nullable;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
